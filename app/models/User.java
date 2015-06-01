d3ed4e2efcb4f162d4d9c305454c3a1a94e2eff3
hhagmans <hendrik.hagmans@web.de>
2015-06-01 16:31:23 +0200

    Fix user table name

--- app/models/User.java
+++ app/models/User.java
@@ -6,10 +6,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import play.db.jpa.JPA;
 
 @Entity
+@Table(name = "\"User\"")
 public class User {
 
 	@Id
